c8094f88d99b8852005d7f586936bf8bf3cdf04f
Manish Verma <31858286+mvermadev@users.noreply.github.com>
2019-05-28 20:09:59 +0530

    Update otp.js

--- routes/otp.js
+++ routes/otp.js
@@ -31,14 +31,14 @@ router.post('/sendMail', (req, res)=>{
     const transporter = nodemailer.createTransport({
         service:'gmail',
         auth: {
-            user: 'manishverma001997@gmail.com',
-            pass: 'm-o-n-u1997ge'
+            user: 'YOUR_EMAIL',
+            pass: 'YOUR_PASSWORD'
         }
     });
     
     const mailOptions ={
-        from : 'manishverma001997@gmail.com',
-        to: email,
+        from : 'YOUR_EMAIL',
+        to: email, // receiver email
         subject: 'OTP',
         text: 'Hey '+ name +', Your OTP Verification is : ' + ran + ' for registration process.'
     }
